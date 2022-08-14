a362f11205509411d4200c89b3b2c917eaa88b96
sultanowskii <dsr431@yandex.ru>
2022-08-14 09:47:34 +0300

    :construction: [style-fix] URLs renamed, code style fixes

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -5,12 +5,12 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-    path('', views.PostIndexView.as_view(), name='post_index'),
-    path('create/', views.create_post, name='post_create'),
-    path('<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('<int:pk>/delete/', views.delete_post, name='post_delete'),
-    path('stat/', views.posts_stat, name='post_stat'),
-    path('<int:pk>/comments/create/', views.post_add_comment, name='post_add_comment'),
-    path('comments/<int:pk>/delete/', views.delete_comment, name='comment_delete'),
+    path('', views.PostIndexView.as_view(), name='index'),
+    path('create/', views.create_post, name='create'),
+    path('<int:pk>/', views.PostDetailView.as_view(), name='detail'),
+    path('<int:pk>/delete/', views.delete_post, name='delete'),
+    path('stat/', views.posts_stat, name='stat'),
+    path('<int:pk>/comments/create/', views.post_add_comment, name='add_comment'),
+    path('comments/<int:pk>/delete/', views.delete_comment, name='delete_comment'),
 ]
 
